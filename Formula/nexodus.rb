82f7bf69e503df69adab64d68257cdc853a981e3
Hiram Chirino <hiram@hiramchirino.com>
2023-04-13 07:57:09 -0400

    Setting up a home-brew tap.

---
+++ Formula/nexodus.rb
@@ -0,0 +1,30 @@
+require "formula"
+
+class Nexodus < Formula
+
+  desc "Secure IP Connectivity for Hybrid Infrastructure and the Edge"
+  homepage "https://nexodus.io/"
+  license "Apache-2.0"
+  head "https://github.com/nexodus-io/nexodus.git", branch: "main"
+
+  version "prod"
+  url "https://github.com/nexodus-io/nexodus/archive/refs/tags/#{version}.tar.gz"
+
+  # TODO: post a checksum of the source tar ball somehere so we don't need update this for each release.
+  # sha256 `curl -L -s https://github.com/nexodus-io/nexodus/some_checksums.txt`.split(' ').first
+
+  depends_on "go@1.20" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    system "go", "build", "-ldflags=-X main.Version=#{version}", "-o", "./dist/nexd", "./cmd/nexd"
+    system "go", "build", "-ldflags=-X main.Version=#{version}", "-o", "./dist/nexctl", "./cmd/nexctl"
+    bin.install "dist/nexd"
+    bin.install "dist/nexctl"
+  end
+
+  test do
+    system "#{bin}/nexctl", "version"
+  end
+
+end
---
+++ README.md
@@ -0,0 +1,8 @@
+# homebrew-nexodus
+
+Homebrew repository to install the Nexodus command line tools.
+
+```sh
+brew tap nexodus-io/nexodus
+brew install nexodus
+```
